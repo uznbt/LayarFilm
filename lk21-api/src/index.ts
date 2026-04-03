b7b769ca7be4fc9dcc65f304bc1bda8983e4d09c
uznbt <uznbt25@gmail.com>
2026-04-04 00:23:13 +0700

    Fix API entry point for Vercel: export default server

--- lk21-api/src/index.ts
+++ lk21-api/src/index.ts
@@ -3,6 +3,10 @@ import server from '@/server';
 
 const port: number = Number(process.env.PORT) || 8080;
 
-server.listen(port, () => {
-    console.log(`[${port}] server running`);
-});
+if (process.env.NODE_ENV !== 'production') {
+    server.listen(port, () => {
+        console.log(`[${port}] server running`);
+    });
+}
+
+export default server;

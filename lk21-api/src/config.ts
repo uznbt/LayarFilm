1ee149cac2fe196744f41c7d15f9eb559dcb81c1
uznbt <uznbt25@gmail.com>
2026-04-06 23:33:48 +0700

    force update mirror for production test

--- lk21-api/src/config.ts
+++ lk21-api/src/config.ts
@@ -10,7 +10,7 @@ const MIRRORS = [
     'https://tv10.lk21official.cc'
 ];
 
-export const LK21_URL = process.env.LK21_URL || MIRRORS[0];
-export const ND_URL = process.env.ND_URL || MIRRORS[0]; 
+export const LK21_URL = 'https://tv3.nontondrama.my';
+export const ND_URL = 'https://tv3.nontondrama.my'; 
 export const PORT = Number(process.env.PORT) || 8080;
 export const NODE_ENV = process.env.NODE_ENV || 'development';
